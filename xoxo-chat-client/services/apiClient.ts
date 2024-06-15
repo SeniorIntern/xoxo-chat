c701226057bc7f5acac3a217cb6735b7558eedc6
SeniorIntern <tnikhil.official@gmail.com>
2024-06-16 01:45:12 +0545

    Implement env variable

--- xoxo-chat-client/services/apiClient.ts
+++ xoxo-chat-client/services/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001/api/v1',
+  baseURL: baseURL,
   withCredentials: true // enable sending cookies with the request
 });
 
